ac76ff3ff664a4ad911eb8a0bcf84f4d90cb8d1f
ian-holdeman <ian.gabircsgo@gmail.com>
2026-08-24 19:12:30 -0600

    first real test

--- src/tests/specs/navigation/navigation.spec.ts
+++ src/tests/specs/navigation/navigation.spec.ts
@@ -1,12 +1,32 @@
 import { test, expect } from '@playwright/test';
-import { HomePage } from '../../pages/home.page';
+import { HomePage, TheBoardPage, TheTestsPage, AboutPage } from '../../pages';
 
 test.describe('Navigation Suite', () => {
   let homePage: HomePage;
+  let boardPage: TheBoardPage;
+  let testsPage: TheTestsPage;
+  let aboutPage: AboutPage;
 
   test.beforeEach(async ({ page }) => {
     homePage = new HomePage(page);
+    boardPage = new TheBoardPage(page);
+    testsPage = new TheTestsPage(page);
+    aboutPage = new AboutPage(page);
+
     await homePage.open();
   });
-});
 
+  test("navigation button validation", async () => {
+    await homePage.header.navBoardBtn.click();
+    await expect(boardPage.pageHeading).toBeVisible();
+
+    await homePage.header.navTestsBtn.click();
+    await expect(testsPage.pageHeading).toBeVisible();
+
+    await homePage.header.navAboutBtn.click();
+    await expect(aboutPage.pageHeading).toBeVisible();
+
+    await homePage.header.navHomeBtn.click();
+    await expect(homePage.heroHeading).toBeVisible();
+  });
+});
